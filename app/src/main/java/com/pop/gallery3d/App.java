3c596d2d1b068e8d39f7e236d788e227beb74743
fupeng <fupeng@songshuyun.com>
2015-12-12 22:15:53 +0800

    add support lib and act main.

---
+++ app/src/main/java/com/pop/gallery3d/App.java
@@ -0,0 +1,13 @@
+package com.pop.gallery3d;
+
+import android.app.Application;
+
+import org.androidannotations.annotations.EApplication;
+
+/**
+ * Created by pengfu on 15/12/12.
+ */
+
+@EApplication
+public class App extends Application {
+}
